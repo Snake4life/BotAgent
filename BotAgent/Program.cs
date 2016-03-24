b7bc3be22f33ca00bd2a8fa69a0b0242eeecd334
Kostia <kostia.shiian@gmail.com>
2016-03-24 17:24:34 +0200

    commit

--- BotAgent/Program.cs
+++ BotAgent/Program.cs
@@ -17,6 +17,8 @@ using log4net;
 
 namespace BotAgent
 {
+    
+
     class Program
     {
         private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
@@ -27,21 +29,31 @@ namespace BotAgent
         static void Main(string[] args)
         {
 
-            Process[] process = Process.GetProcessesByName(ConfigurationManager.AppSettings["FulcrumBot"]);
 
-            if (process.Length == 0)
+            Process[] process = Process.GetProcessesByName(ConfigurationManager.AppSettings["botName"]);
+
+            while (process.Length == 0) 
             {
+                
                 // we haven't found any proccess with desired name, so we need to sleep
+                log.Warn(String.Format("Process with name {0} is not found, waiting {1} ms", ConfigurationManager.AppSettings["botName"],
+                    ConfigurationManager.AppSettings["sleepIntervaIfBotDidntFound"])
+                    );
 
                 Thread.Sleep(Int32.Parse(ConfigurationManager.AppSettings["sleepIntervaIfBotDidntFound"]));
-
                 
-            }
+                process = Process.GetProcessesByName(ConfigurationManager.AppSettings["botName"]);
+
+            } 
             // there can be only one proccess of FulcrumBot
 
             var mainWindow = AutomationElement.FromHandle(process[0].MainWindowHandle);
 
-            log.Info("The MAX waiting counter value is readched for checking whether the .\\core directory exists");
+            log.Info(String.Format("Found {0} process, pid={1}",
+                ConfigurationManager.AppSettings["botName"],
+                process[0].Id
+                )
+                );
 
 //            Console.ReadLine();
         }
