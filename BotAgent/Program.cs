e747b050c358eeb4e96eb30eae396c0177cd9651
Kostia <kostia.shiian@gmail.com>
2016-03-24 17:09:41 +0200

    commit

--- BotAgent/Program.cs
+++ BotAgent/Program.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 using System.Diagnostics;
+using System.Configuration;
+
 
 using System.Windows.Automation;
 
@@ -24,11 +27,21 @@ namespace BotAgent
         static void Main(string[] args)
         {
 
-            Process[] process = Process.GetProcessesByName("FulcrumBot");
+            Process[] process = Process.GetProcessesByName(ConfigurationManager.AppSettings["FulcrumBot"]);
 
+            if (process.Length == 0)
+            {
+                // we haven't found any proccess with desired name, so we need to sleep
+
+                Thread.Sleep(Int32.Parse(ConfigurationManager.AppSettings["sleepIntervaIfBotDidntFound"]));
+
+                
+            }
             // there can be only one proccess of FulcrumBot
 
             var mainWindow = AutomationElement.FromHandle(process[0].MainWindowHandle);
+
+            log.Info("The MAX waiting counter value is readched for checking whether the .\\core directory exists");
 
 //            Console.ReadLine();
         }
